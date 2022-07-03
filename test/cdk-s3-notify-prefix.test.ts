84ccf2d5a7695155a2cec803252d7e169c3e2abb
suzukiken <suzukiken@gmail.com>
2022-07-03 05:58:50 +0000

    Initial commit

---
+++ test/cdk-s3-notify-prefix.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkS3NotifyPrefix from '../lib/cdk-s3-notify-prefix-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-s3-notify-prefix-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkS3NotifyPrefix.CdkS3NotifyPrefixStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
